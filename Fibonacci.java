0410004963e0b78187cdec9db79f0e0ca3001a6d
Phillip Yohner <phillip.n.yohner@disney.com>
2025-01-08 12:22:01 -0500

    Created Fibonacci class that includes fibonacci calculation and main methods.

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 10; // The term to find in the Fibonacci sequence
+        int result = fibonacci(n);
+        System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
+    }
+
+}
